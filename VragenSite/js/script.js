62bc6a07369f6c11fcfecd44e1ee6714e0508be0
Lex van den Brandt <info@vdbrandtmedia.nl>
2016-06-02 15:01:58 +0200

    js added for question transition

--- VragenSite/js/script.js
+++ VragenSite/js/script.js
@@ -60,13 +60,70 @@ $( document ).ready(function() {
     console.log($("#q3 h2").text());
     console.log($(this).text());
 
-    question2 = $("#q3 h2").text();
-    answer2 =   $(this).text();
+    question3 = $("#q3 h2").text();
+    answer3 =   $(this).text();
 
     $( "#q3" ).delay( 500 ).fadeOut( "slow", function() {});
 
     $("#q4 h1").append("Question 4");
     $( "#q4" ).delay( 1500 ).fadeIn(  "slow", function() {});
+  });
+
+
+  $( "#q4 .answerPart" ).click(function(){
+    console.log("Clicked");
+    console.log($("#q4 h2").text());
+    console.log($(this).text());
+
+    question4 = $("#q4 h2").text();
+    answer4 =   $(this).text();
+
+    $( "#q4" ).delay( 500 ).fadeOut( "slow", function() {});
+
+    $("#q5 h1").append("Question 5");
+    $( "#q5" ).delay( 1500 ).fadeIn(  "slow", function() {});
+  });
+
+  $( "#q5 .answerPart" ).click(function(){
+    console.log("Clicked");
+    console.log($("#q5 h2").text());
+    console.log($(this).text());
+
+    question5 = $("#q5 h2").text();
+    answer5 =   $(this).text();
+
+    $( "#q5" ).delay( 500 ).fadeOut( "slow", function() {});
+
+    $("#q6 h1").append("Question 6");
+    $( "#q6" ).delay( 1500 ).fadeIn(  "slow", function() {});
+  });
+
+  $( "#q6 .answerPart" ).click(function(){
+    console.log("Clicked");
+    console.log($("#q6 h2").text());
+    console.log($(this).text());
+
+    question6 = $("#q6 h2").text();
+    answer6 =   $(this).text();
+
+    $( "#q6" ).delay( 500 ).fadeOut( "slow", function() {});
+
+    $("#q7 h1").append("Question 7");
+    $( "#q7" ).delay( 1500 ).fadeIn(  "slow", function() {});
+  });
+
+  $( "#q7 .answerPart" ).click(function(){
+    console.log("Clicked");
+    console.log($("#q7 h2").text());
+    console.log($(this).text());
+
+    question7 = $("#q7 h2").text();
+    answer7 =   $(this).text();
+
+    $( "#q7" ).delay( 500 ).fadeOut( "slow", function() {});
+
+    $("#q8 h1").append("Question 5");
+    $( "#q8" ).delay( 1500 ).fadeIn(  "slow", function() {});
   });
 
 
