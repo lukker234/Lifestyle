24f6066beccdcd5d76c1e8cf482faca57f2cdc1b
Lukker234 <l.daalmeijer@student.fontys.nl>
2016-06-17 13:03:09 +0200

    d

--- led test/index.php
+++ led test/index.php
@@ -1,7 +1,7 @@
 
 <?php
 exec("mode com4: BAUD=9600 PARITY=n DATA=8 STOP=1 to=off dtr=off rts=off");
-$fp =fopen("com3", "w");
+$fp =fopen("com3", "w+");
 //$fp = fopen('/dev/ttyUSB0','r+'); //use this for Linux
 fwrite($fp, "3"); //write string to serial
 fclose($fp);
